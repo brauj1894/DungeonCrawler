2da651b7b95608eae7859f2fd9db4d2d4e3a296f
joel <brauj1894@wrdsb.ca>
2018-09-16 22:44:16 -0400

    Added Screen Switching Added Game Screen

--- core/src/com/dungeoncrawler/DungeonCrawler.java
+++ core/src/com/dungeoncrawler/DungeonCrawler.java
@@ -1,33 +1,48 @@
 package com.dungeoncrawler;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class DungeonCrawler extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.dungeoncrawler.screens.ScrGame;
+
+
+public class DungeonCrawler extends Game {
+
+	// Width and Height Of Screen
+	public int nWidth, nHeight;
+
+	// Screens
+	public ScrGame scrGame;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Gets the width and height
+		nWidth = Gdx.graphics.getWidth();
+		nHeight = Gdx.graphics.getHeight();
+
+		// Create Screens
+		scrGame = new ScrGame(this);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	}
+
+	public void changeScreen(int nScreen) {
+		switch (nScreen) {
+			case 0:
+				// Game
+				setScreen(scrGame);
+				break;
+			default:
+				System.out.println("ERROR: INVALID SCREEN ID '"+nScreen+"' Could not switch to that screen");
+				break;
+		}
 	}
 }
