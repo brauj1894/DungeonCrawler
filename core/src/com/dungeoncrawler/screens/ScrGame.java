a09930118476fee2486a8ce3f47498dcae27242b
joel <brauj1894@wrdsb.ca>
2018-09-16 22:48:35 -0400

    Fixed Game Screen Should open a small blue window

--- core/src/com/dungeoncrawler/screens/ScrGame.java
+++ core/src/com/dungeoncrawler/screens/ScrGame.java
@@ -1,6 +1,8 @@
 package com.dungeoncrawler.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.dungeoncrawler.DungeonCrawler;
 
 public class ScrGame implements Screen{
@@ -18,7 +20,8 @@ public class ScrGame implements Screen{
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
